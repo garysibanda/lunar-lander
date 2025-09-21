fbc523505eb7264c5dabaed7def0cde005e8539f
Gary Sibanda <sibandamthandazo@gmail.com>
2025-09-20 22:21:44 -0600

    initial commit

---
+++ LunarLander/LunarLander/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LunarLander
+//
+//  Created by Gary Sibanda on 9/20/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
